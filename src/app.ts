4ddece65ebf99204983eaba7e217dd3a79d94483
Andres Rivero <andres19rivero@gmail.com>
2024-03-24 21:00:07 -0400

    feat: implement initial api server skeleton

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log('Hello World!')
+import { envs } from "./config/envs";
+import { AppRoutes } from "./presentation/routes";
+import { Server } from "./presentation/server";
+
+(async () => {
+  main();
+})();
+
+function main() {
+  const server = new Server({
+    port: envs.PORT,
+    routes: AppRoutes.routes,
+  });
+
+  server.start();
+}
